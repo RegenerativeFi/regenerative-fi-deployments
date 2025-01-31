f31af04d197a9f54db5e597c7ceb506a11471791
stephen <991266+stxphxn@users.noreply.github.com>
2025-01-31 13:38:59 +0000

    feat: add gauge registry

--- tasks/20241230-regenerative-gauge-registry/index.ts
+++ tasks/20241230-regenerative-gauge-registry/index.ts
@@ -4,10 +4,5 @@ import { RegenerativeGaugeRegistryDeployment } from './input';
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as RegenerativeGaugeRegistryDeployment;
 
-  await task.deployAndVerify(
-    'ChildChainGaugeRegistry',
-    [input.L2BalancerPseudoMinter, input.ChildChainLiquidityGaugeFactory],
-    from,
-    force
-  );
+  await task.deployAndVerify('RegenerativeGaugeRegistry', [input.Vault], from, force);
 };
